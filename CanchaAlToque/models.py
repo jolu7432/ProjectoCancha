9e4c423e304ea7cd4592a3719ba5cdb0e5ad5ec3
valtanders <brunobollati@gmail.com>
2016-04-09 13:02:33 -0300

    primeros modelos y admin.py

--- CanchaAlToque/models.py
+++ CanchaAlToque/models.py
@@ -2,4 +2,34 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Complejo(models.Model):
+    """clase Complejo"""
+    nombre = models.CharField(max_length=200)
+    direccion = models.CharField(max_length=250)
+    #zona = model.ForeignKey(Zona)
+    #localidad = model.ForeignKey(Localidad)
+    #provincia = model.ForeignKey(Provincia)
+
+    def __str__(self):
+        return self.nombre
+
+class Cancha(models.Model):
+    """clase Cancha"""
+    nombre = models.CharField(max_length=200)
+    esCubierto = models.BooleanField()
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class CaracteristicaChancha(models.Model):
+    cancha = models.ForeignKey(Cancha)
+    caracteristica = models.ForeignKey(Caracteristicas)
+
+
+class Caracteristicas(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
