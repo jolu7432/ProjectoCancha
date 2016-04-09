cecb4884ab1bc3a4a534a830e1afd3b52e8bc014
Jorge <jorgeluis.tanos@exitco.biz>
2016-04-09 13:09:47 -0300

    modelos

--- CanchaAlToque/models.py
+++ CanchaAlToque/models.py
@@ -3,18 +3,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Complejo(models.Model):
-    """clase Complejo"""
     nombre = models.CharField(max_length=200)
     direccion = models.CharField(max_length=250)
-    #zona = model.ForeignKey(Zona)
-    #localidad = model.ForeignKey(Localidad)
-    #provincia = model.ForeignKey(Provincia)
+    zona = models.ForeignKey(Zona)
+    localidad = models.ForeignKey(Localidad)
+    provincia = models.ForeignKey(Provincia)
 
     def __str__(self):
         return self.nombre
 
 class Cancha(models.Model):
-    """clase Cancha"""
     nombre = models.CharField(max_length=200)
     esCubierto = models.BooleanField()
     cantidad = models.IntegerField()
@@ -33,8 +31,6 @@ class Caracteristicas(models.Model):
     def __str__(self):
         return self.nombre
 
-# Create your models here.
-
 class Usuario(User):
     esCapitan = models.BooleanField()
     def __str__(self):
@@ -52,16 +48,16 @@ class Provincia(models.Model):
 
 
 class Turno(models.Model):
-    #cancha = models.ForeignKey(Cancha)
+    cancha = models.ForeignKey(Cancha)
     comienzo = models.DateTimeField()
     fin = models.DateTimeField()
     esActivo = models.BooleanField()
     esCancelado = models.BooleanField()
     esPerdido = models.BooleanField()
     esFijo = models.BooleanField()
-    seña = models.ForeignKey(Seña)
+    senia = models.ForeignKey(Senia)
 
-class Seña(models.Model):
+class Senia(models.Model):
     monto = models.FloatField()
     vencimiento = models.DateTimeField()
 
